fbdfed16cbe7076b6be9cf80b335fd35420b5558
locdangxuan <xuanloc20041998@gmail.com>
2019-11-30 18:21:42 +0700

    view engine

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
-// This file starts the both the Express server, used to serve the actual webpage,
-// and the Socket.io server, used to handle the the realtime connection to the client.
-
 var express = require("express");
 var app = express();
 var http = require("http").Server(app);
 var io = require("./libs/game_manager").listen(http);  // Start Socket.io server and let game_manager handle those connections
 
 app.set("port", (process.env.PORT || 3001));  // Use either given port or 3001 as default
+app.set('view engine', 'pug');
+app.set('views', './views');
+
 app.use(express.static("public"));  // Staticly serve pages, using directory 'public' as root 
 
 // User connects to server
 app.get("/", function(req, res) {
   // Will serve static pages, no need to handle requests
+  res.render('index');
 });
 
 // If any page not handled already handled (ie. doesn't exists)
